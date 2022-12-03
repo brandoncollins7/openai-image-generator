41437940252556d49d75356e158cb91f44880f22
Brandon Collins <brandon.collins@frnt.io>
2022-12-03 13:37:41 -0500

    feat: add text generation

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ const core = require('@actions/core');
         apiKey: apiKey
     });
     const openai = new OpenAIApi(configuration);
-    const response = await openai.createImage({
-        prompt: query,
-        n: 1,
-        size: core.getInput('size'),
-    });
-    core.setOutput("url", response.data.data[0].url);
+    const type = core.getInput('type');
+    if (type === "image") {
+        const response = await openai.createImage({
+            prompt: query,//"a sad cartoon penguin in the style of family guy",
+            n: 1,
+            size: core.getInput('image_size'),
+        });
+        core.setOutput("url", response.data.data[0].url);
+    } else if (type === "text") {
+        const response = await openai.createCompletion({
+            prompt: query,
+            model: core.getInput('model'),
+            n: 1
+        });
+        core.setOutput("url", response.data.choices[0].text);
+    }
 })()
